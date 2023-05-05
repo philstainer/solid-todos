4e3b14920c77cade1aefc2a6ec964e8dc8195843
Phil Stainer <philstainer@hollandandbarrett.com>
2023-05-05 11:43:15 +0100

    set base in vite.config

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,13 @@
-import { defineConfig } from 'vite';
-import solidPlugin from 'vite-plugin-solid';
+import { defineConfig } from "vite";
+import solidPlugin from "vite-plugin-solid";
 
 export default defineConfig({
+  base: "/solid-todos/",
   plugins: [solidPlugin()],
   server: {
     port: 3005,
   },
   build: {
-    target: 'esnext',
+    target: "esnext",
   },
 });
